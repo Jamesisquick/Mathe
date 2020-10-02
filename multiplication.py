952106ec350089fc18eb7b1233cb471f930d5e0a
Jamesisquick <754425@dpsk12.et>
2020-10-02 13:13:37 -0600

    fixed assertion error

--- multiplication.py
+++ multiplication.py
@@ -4,5 +4,5 @@ def times_x_y(x, y):
 def test_x_y():
 	assert times_x_y(3, 10) == 30
 	assert times_x_y(30, 30) == 900
-	assert times_x_y(3, 7) == 22
+	assert times_x_y(3, 7) == 21
 	assert times_x_y(2, 0) == 0
